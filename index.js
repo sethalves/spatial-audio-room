6ec8211a53272e02b1edd2077faded1a2077456f
Ken Cooke <ken@highfidelity.io>
2022-03-28 14:38:24 -0700

    Fix poor performance of Math.atan2() with fast approximation

--- index.js
+++ index.js
@@ -117,13 +117,40 @@ function setThreshold(value) {
     }
 }
 
+// Fast approximation of Math.atan2(y, x)
+// rel |error| < 4e-5, smooth (exact at octant boundary)
+// for y=0 x=0, returns NaN
+function fastAtan2(y, x) {
+    let ax = Math.abs(x);
+    let ay = Math.abs(y);
+    let x1 = Math.min(ax, ay) / Math.max(ax, ay);
+
+    // 9th-order odd polynomial approximation to atan(x) over x=[0,1]
+    // evaluate using Estrin's method
+    let x2 = x1 * x1;
+    let x3 = x2 * x1;
+    let x4 = x2 * x2;
+    let r =  0.024065681985187 * x4 + 0.186155334995372;
+    let t = -0.092783165661197 * x4 - 0.332039687921915;
+    r = r * x2 + t;
+    r = r * x3 + x1;
+
+    // use octant to reconstruct result in [-PI,PI]
+    if (ay > ax) r = 1.570796326794897 - r;
+    if (x < 0.0) r = 3.141592653589793 - r;
+    if (y < 0.0) r = -r;
+    return r;
+}
+
 function setPosition(hifiSource) {
     let dx = hifiSource._x - hifiListener._x;
     let dy = hifiSource._y - hifiListener._y;
 
     //let azimuth = angle_wrap(atan2f(dx, dy) - avatarOrientationRadians);
-    let azimuth = Math.atan2(dx, dy);
-    let distance = Math.sqrt(dx * dx + dy * dy);
+
+    let distanceSquared = dx * dx + dy * dy;
+    let distance = Math.sqrt(distanceSquared);
+    let azimuth = (distanceSquared < 1e-30) ? 0.0 : fastAtan2(dx, dy);
 
     hifiSource.parameters.get('azimuth').value = azimuth;
     hifiSource.parameters.get('distance').value = distance;
